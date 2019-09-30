38c25f2d451c2fe84103f2cc7d0c1abc6c3b98b5
freddy <freddy@oneledger.io>
2019-09-30 12:32:19 -0400

    add detail field for every error object

--- errorType.js
+++ errorType.js
@@ -1,32 +1,69 @@
 const requestErrors = {
-    WrongPassword: {code: -11000, message: "Wrong password"},
-    InvalidKeyIndex: {code: -11001, message: "invalid keyIndex, keyIndex should should be a non-negative integer"},
-    InvalidEncryptedMasterKeySeed: {code: -11002, message: "invalid encrypted master key seed"},
-    InvalidEncodedTxMessage: {code: -11003, message: "invalid encoded tx message"},
-    InvalidBTCtxMessage: {code: -11004, message: "invalid BitCoin tx message"},
-    InvalidNonce: {code: -11005, message: "invalid nonce, nonce should be a non-negative integer"},
-    InvalidGasPrice: {code: -11006, message: "invalid gasPrice, gasPrice should be a positive number"},
-    InvalidTxValue: {code: -11007, message: "invalid tx value, value should be a positive number"},
-    InvalidGasLimit: {code: -11008, message: "invalid gasLimit, gasLimit should be a positive integer"},
+    WrongPassword: {code: -11000, message: "Wrong password", detail: "-11000 : Wrong password"},
+    InvalidKeyIndex: {
+        code: -11001,
+        message: "Invalid keyIndex, keyIndex should should be a non-negative integer",
+        detail: "-11001 : invalid keyIndex"
+    },
+    InvalidEncryptedMasterKeySeed: {
+        code: -11002,
+        message: "Invalid encrypted master key seed",
+        detail: "-11002 : invalid encrypted master key seed"
+    },
+    InvalidEncodedTxMessage: {
+        code: -11003,
+        message: "Invalid encoded tx message",
+        detail: "-11003 : invalid encoded tx message"
+    },
+    InvalidBTCtxMessage: {
+        code: -11004,
+        message: "Invalid BitCoin tx message",
+        detail: "-11004 : invalid BitCoin tx message"
+    },
+    InvalidNonce: {
+        code: -11005,
+        message: "Invalid nonce, nonce should be a non-negative integer",
+        detail: "-11005 : invalid nonce"
+    },
+    InvalidGasPrice: {
+        code: -11006,
+        message: "Invalid gasPrice, gasPrice should be a positive number",
+        detail: "-11006 : invalid gasPrice"
+    },
+    InvalidTxValue: {
+        code: -11007,
+        message: "Invalid tx value, value should be a positive number",
+        detail: "-11007 : invalid tx value"
+    },
+    InvalidGasLimit: {
+        code: -11008,
+        message: "Invalid gasLimit, gasLimit should be a positive integer",
+        detail: "-11008 : invalid gasLimit"
+    },
     InvalidTxReceiverAddress: {
         code: -11009,
-        message: "invalid tx receiver address, address should have 40 characters, please include 0x as the prefix"
+        message: "Invalid tx receiver address, address should have 40 characters, please include 0x as the prefix",
+        detail: "-11009 : invalid tx receiver address"
     },
     InvalidDerivedKeyType: {
         code: -11010,
-        message: "Wrong key type for deriving new key, valid key type should be either OLT, BTC-P2PK, BTC-P2PKH or ETH"
+        message: "Invalid key type for deriving new key, valid key type should be either OLT, BTC-P2PK, BTC-P2PKH or ETH",
+        detail: "-11010 : invalid key type for deriving new key"
     },
     InvalidSignKeyType: {
         code: -11011,
-        message: "Wrong key type for signing tx, valid key type should be either OLT, BTC or ETH"
+        message: "Invalid key type to sign tx, valid key type should be either OLT, BTC or ETH",
+        detail: "-11011 : invalid key type to sign tx"
     },
     InvalidBTCNetworkType: {
         code: -11012,
-        message: "Wrong BitCoin network type, valid network type should be either BITCOIN, TESTNET or REGTEST"
+        message: "Invalid BitCoin network type, valid network type should be either BITCOIN, TESTNET or REGTEST",
+        detail: "-11012 : invalid BitCoin network type"
     },
     InvalidETHSignature: {
         code: -11013,
-        message: "Invalid ETH tx signature"
+        message: "Invalid ETH tx signature",
+        detail: "-11013 : invalid ETH tx signature"
     }
 };
 
