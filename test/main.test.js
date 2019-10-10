2145ac4277dfb86cef8cc58395a4b866d61b17e9
freddy <freddy@oneledger.io>
2019-10-10 11:52:03 -0400

    change get master key seed info funtion and change example

--- test/main.test.js
+++ test/main.test.js
@@ -25,27 +25,33 @@ const mnemonicArray = [{index: 1, word: 'turtle'},
     {index: 23, word: 'have'},
     {index: 24, word: 'cage'}];
 
-const encryptedMasterKeySeed = '{"iv":"QlG4nschGJ05XE5eewWNoQ==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"0S3l9i0pRtQFaAZFTi0nHw==","ct":"oqwz5Fan11XtWeUCK2UCaVmlfJKHNuOjAmgLZsQOaedukydoZdUta6iK5ZV6KQUn4iuWuenIDvAYuTchadsh0z3FQC9NHwYTNqCISxKhe1TeBLwP4V7DVQwvdQJlKwBg"}\n';
+const encryptedMasterKeySeedExample = '{"iv":"QlG4nschGJ05XE5eewWNoQ==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"0S3l9i0pRtQFaAZFTi0nHw==","ct":"oqwz5Fan11XtWeUCK2UCaVmlfJKHNuOjAmgLZsQOaedukydoZdUta6iK5ZV6KQUn4iuWuenIDvAYuTchadsh0z3FQC9NHwYTNqCISxKhe1TeBLwP4V7DVQwvdQJlKwBg"}\n';
 
 describe("examples of how to use HD-vault", function () {
     it("test 1", async function () {
         // generate 24 mnemonic
         const words = HDVault.mnemonicUtil.mnemonicGenerator24();
+
         // generate masterKeySeed with mnemonic and password
         const mm = new HDVault.MasterKeySeedManager(words, "123456");
+
         // when import masterKeySeed file, unlock it with password
-        const unlockResult = await HDVault.masterKeySeedUtil.unlockMasterKeySeed("123456", encryptedMasterKeySeed).catch(err => {
+        const unlockResult = await HDVault.masterKeySeedUtil.unlockMasterKeySeed("123456", encryptedMasterKeySeedExample).catch(err => {
             console.log("unlock masterKeySeed error: ", err);
         });
         console.log("unlock masterKeySeed result: ", unlockResult);
+
         // get masterKeySeed info after new masterKeySeed is generated OR when user recovery with new password
-        mm.getMasterKeySeedInfo((encryptedMasterKeySeed, masterKeySeedAddress) => {
-            console.log("get masterKeySeed info: ", encryptedMasterKeySeed, masterKeySeedAddress);
-        });
+        const {encryptedMasterKeySeed, masterKeySeedAddress} = mm.getMasterKeySeedInfo();
+        console.log("get masterKeySeed info: ", encryptedMasterKeySeed, masterKeySeedAddress);
+
         // when user recovery masterKeySeed, get the masterAddress based on provided mnemonic for comparison
         console.log("recovery address: " + HDVault.masterKeySeedUtil.getMasterKeySeedAddressForRecovery(mnemonicArray));
+
         // derive new key with different keyType : OLT, BTC-P2PK, BTC-P2PKH, ETH
         // return new key's address and keyIndex to store locally
+        // also return publicKey for broadcasting tx
+        // Notice : only OLT, ETH and BTC-P2PKH return keyIndex, address and publicKey, BTC-P2PK only return keyIndex, publicKey
         const derivedData = {
             keyType: "OLT",
             keyIndex: 0,
@@ -57,6 +63,7 @@ describe("examples of how to use HD-vault", function () {
             console.log("get derived key's address: ", address);
             console.log("get derived key's public key: ", publicKey);
         });
+
         // sign tx with derived new key
         const messageOLT = 'eyJ0eF90eXBlIjoyLCJ0eF9kYXRhIjoiZXlKUGQyNWxjaUk2SWpCNE1qZ3dabVkxT0dNMk5UYzNaRGhrWkRBeE5XVmlNelkzTUdRMU1UY3paVFl4WVRnNE1HWmxZU0lzSWtGalkyOTFiblFpT2lJd2VESTRNR1ptTlRoak5qVTNOMlE0WkdRd01UVmxZak0yTnpCa05URTNNMlUyTVdFNE9EQm1aV0VpTENKT1lXMWxJam9pZEdWemRHUnZiV0ZwYmpFeElpd2lVSEpwWTJVaU9uc2lZM1Z5Y21WdVkza2lPaUpQVEZRaUxDSjJZV3gxWlNJNklqRXdNREF3TURBd01EQXdNREF3TURBd01EQXdNREFpZlgwPSIsImZlZSI6eyJQcmljZSI6eyJjdXJyZW5jeSI6Ik9MVCIsInZhbHVlIjoiMTAwMDAwMDAwMDAwMDAwMDAwMCJ9LCJHYXMiOjF9LCJtZW1vIjoiNGM1MzQ4ZTctYWNjOS0xMWU5LTlhN2MtNDIwMTBhMGEwMDA5In0=';
         const signData = {
@@ -70,6 +77,7 @@ describe("examples of how to use HD-vault", function () {
             // console.log(error);
             console.log("signature from derived key: ", signature);
         });
+
         // verify address
         const addreVerifyResult = HDVault.address.verify("1BRpDq7Px6X4k5hN4Q6jFkBypFMizf64Yg", "OLT");
         console.log(addreVerifyResult);
