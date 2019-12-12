95e0f6ed8bbb7195d2ba10f363df5e37a2ded5a1
freddy <freddy@oneledger.io>
2019-12-12 16:35:29 -0500

    refact index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,46 @@
-import {verifyMnemonic, mnemonicGenerator12, mnemonicGenerator24, unlockMasterKeySeed, getMasterKeySeedAddressForRecovery} from "./masterKeySeed";
-exports.MasterKeySeedManager = require("./masterKeySeedManager");
-exports.derivedKeyManager = require("./derivedKeyManager");
-exports.masterKeySeedUtil = {
-    unlockMasterKeySeed,
-    getMasterKeySeedAddressForRecovery
-};
-exports.mnemonicUtil = {
+import {
     verifyMnemonic,
     mnemonicGenerator12,
-    mnemonicGenerator24
-};
-const {verify} = require("./addressVerifier");
-exports.address = {
-    verify
-};
-const {derivedKeyType} = require("./config");
-exports.CONSTANT = {
-    KeyType: derivedKeyType
-};
+    mnemonicGenerator24,
+    unlockMasterKeySeed,
+    getMasterKeySeedAddressForRecovery
+} from "./masterKeySeed";
+import {verify} from "./addressVerifier";
+import {derivedKeyType} from "./config";
+import MasterKeySeedManager = require("./masterKeySeedManager");
+import derivedKeyManager = require("./derivedKeyManager");
+
+export declare namespace masterKeySeedUtil {
+    export {
+        unlockMasterKeySeed,
+        getMasterKeySeedAddressForRecovery
+    }
+}
+
+export declare namespace mnemonicUtil {
+    export {
+        verifyMnemonic,
+        mnemonicGenerator12,
+        mnemonicGenerator24
+    }
+}
+
+export declare namespace address {
+    export {
+        verify
+    }
+}
+
+export declare namespace CONSTANT {
+    export {
+        derivedKeyType as KeyType
+    }
+}
+
+export function MasterKeySeedManager();
+
+export module derivedKeyManager {
+    export function deriveNewKeyPair()
+    export function signTx()
+}
+
