7b2efa4ca37061a5539b48a66d8ff573c91551e7
freddy <freddy@oneledger.io>
2019-12-13 12:48:34 -0500

    restruct index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -10,58 +10,35 @@ import {derivedKeyType} from "./config";
 import MasterKeySeedManager = require("./masterKeySeedManager");
 import derivedKeyManager = require("./derivedKeyManager");
 
-export declare namespace masterKeySeedUtil {
-    function unlockMasterKeySeed(password: string, encryptedMasterKeySeed: string): Promise<boolean | Object>
-
-    function getMasterKeySeedAddressForRecovery(mnemonicArray: array<{ index: number, word: string }>): string
-}
-
-export declare namespace mnemonicUtil {
-    function verifyMnemonic(mnemonicArray: array<{ index: number, word: string }>): boolean
-
-    function mnemonicGenerator12(): array<{ index: number, word: string }>
-
-    function mnemonicGenerator24(): array<{ index: number, word: string }>
-}
-
-export declare enum derivedKeyType {
-    OLT = "OLT",
-    BTCP2PK = "BTCP2PK",
-    BTCP2PKH = "BTCP2PKH",
-    ETH = "ETH"
-}
-
-export declare namespace address {
-    function verify(address: string, addressType: derivedKeyType): Promise<boolean | error>
-}
-
-declare namespace CONSTANT {
-    export {
-        derivedKeyType as KeyType
-    }
-}
-
 export class MasterKeySeedManager {
     encryptedMasterKeySeed: string;
     masterKeySeedAddress: string;
 
-    constructor(mnemonicArray: Array<{ index: number, word: string }>, password: string) {
+    constructor(mnemonicArray: Array<{ index: number, word: string }>, password: string);
+    getMasterKeySeedInfo();
+    unlockMasterKeySeed(password: string)
+}
+
+export declare module derivedKeyManager {
+    export declare enum derivedKeyType {
+        OLT = "OLT",
+        BTCP2PK = "BTCP2PK",
+        BTCP2PKH = "BTCP2PKH",
+        ETH = "ETH"
     }
-}
 
-export declare enum BitCoinNetwork {
-    BTCOIN = "BTCOIN",
-    TESTNET = "TESTNET",
-    REGTEST = "REGTEST"
-}
+    export declare enum txSignKeyType {
+        OLT = "OLT",
+        BTC = "BTC ",
+        ETH = "ETH"
+    }
 
-export declare enum txSignKeyType {
-    OLT = "OLT",
-    BTC = "BTC ",
-    ETH = "ETH"
-}
+    export declare enum BitCoinNetwork {
+        BTCOIN = "BTCOIN",
+        TESTNET = "TESTNET",
+        REGTEST = "REGTEST"
+    }
 
-export declare namespace derivedKeyManager {
     export interface deriveKeyParamsInterface {
         keyType: derivedKeyType,
         keyIndex: number,
@@ -83,4 +60,25 @@ export declare namespace derivedKeyManager {
 
     function signTx({message, keyType, keyIndex, password, encryptedMasterKeySeed, network}: signTxInterface): Promise<response | error>;
 }
+
+export declare module masterKeySeedUtil {
+    function unlockMasterKeySeed(password: string, encryptedMasterKeySeed: string): Promise<boolean | Object>
+
+    function getMasterKeySeedAddressForRecovery(mnemonicArray: array<{ index: number, word: string }>): string
+}
+
+export declare module mnemonicUtil {
+    function verifyMnemonic(mnemonicArray: array<{ index: number, word: string }>): boolean
+
+    function mnemonicGenerator12(): array<{ index: number, word: string }>
+
+    function mnemonicGenerator24(): array<{ index: number, word: string }>
+}
+
+export declare module address {
+    function verify(address: string, addressType: derivedKeyManager.derivedKeyType): Promise<boolean | error>
+}
+
+
+
 
