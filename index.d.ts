033ff018adbeac0dd4249f940940afe31f6fb6ee
freddyli7 <704657512@qq.com>
2020-10-09 16:19:36 -0400

    minor fix

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 import {newWallet, passwordGenerator, getPassword, passwordDefaultFormat} from "./localWallet";
 
 export namespace Wallet {
-    function newWallet(): string;
+    function newWallet(password: string): string;
 }
 
 export namespace Password {
-    function passwordGenerator(): string;
+    function passwordGenerator(password: string, account: string): string;
 
-    function getPassword(): string;
+    function getPassword(password: string, account: string): string;
 }
 
 export const Config = {
